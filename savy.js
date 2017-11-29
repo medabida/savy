86b7667e3b682bd04aba7ef75da8d5f4b573cdbc
medabida <medabida2428@gmail.com>
2017-11-29 23:15:31 +0100

    add callback when data is loaded or destroyed

--- savy.js
+++ savy.js
@@ -1,8 +1,7 @@
 (function($) {
-  $.fn.savy = function(order) {
+  $.fn.savy = function(order,fn) {
     if (order == "load") {
       $(this).each(function() {
-
         if($(this).is("input") || $(this).is("textarea")) {
           if(localStorage.getItem("savy-"+this.id)){
             this.value = localStorage.getItem("savy-"+this.id);
@@ -29,12 +28,18 @@
           }
         }
       });
+      if ($.isFunction(fn)) {
+        fn();
+      }
     }else if (order == "destroy") {
       $(this).each(function() {
         if(localStorage.getItem("savy-"+this.id)){
           localStorage.removeItem("savy-"+this.id)
         }
       });
+      if ($.isFunction(fn)) {
+        fn();
+      }
     }else{
       console.error("savy action not defined please use $('.classname').savy('load') to trigger savy to save all inputs")
     }
