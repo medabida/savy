4c3fb1557687d1d5231e81e9ba5d13f323de7683
medabida <medabida2428@gmail.com>
2017-12-29 12:54:34 +0100

    bug fixes

--- savy.js
+++ savy.js
@@ -1,67 +1,59 @@
 (function($) {
   $.fn.savy = function(order,fn) {
+    const sv = "savy-";
     if (order == "load") {
       $(this).each(function() {
-
         if ($(this).is(":radio")) {
-          if(localStorage.getItem("savy-"+$(this).attr("name"))){
-            if (localStorage.getItem("savy-"+$(this).attr("name")) == this.id) {
+          if(localStorage.getItem(sv+$(this).attr("name"))){
+            if (localStorage.getItem(sv+$(this).attr("name")) == this.id) {
               this.checked = true;
             }else{
               this.checked = false
             }
           }
           $(this).change(function() {
-            localStorage.setItem("savy-"+$(this).attr("name"), this.id);
+            localStorage.setItem(sv+$(this).attr("name"), this.id);
           });
-
-        }
-        else if($(this).is(":checkbox")){
-          if(localStorage.getItem("savy-"+this.id)){
-            this.checked = (localStorage.getItem("savy-"+this.id) == "1" ? true : false);
+        }else if($(this).is(":checkbox")){
+          if(localStorage.getItem(sv+this.id)){
+            this.checked = (localStorage.getItem(sv+this.id) == "1" ? true : false);
           }
           $(this).change(function() {
-            localStorage.setItem("savy-"+this.id, (this.checked ? "1" : "0"));
+            localStorage.setItem(sv+this.id, (this.checked ? "1" : "0"));
           });
-        }
-        else if($(this).is("input") || $(this).is("textarea")) {
-          if(localStorage.getItem("savy-"+this.id)){
-            this.value = localStorage.getItem("savy-"+this.id);
+        }else if($(this).is("input") || $(this).is("textarea")) {
+          if(localStorage.getItem(sv+this.id)){
+            this.value = localStorage.getItem(sv+this.id);
           }
           $(this).keyup(function() {
-            localStorage.setItem("savy-"+this.id, this.value);
+            localStorage.setItem(sv+this.id, this.value);
           });
-        }
-        else if($(this).is("select")) {
+        }else if($(this).is("select")) {
           if ($(this).is("[multiple]")) {
-            if(localStorage.getItem("savy-"+this.id)){
-              $(this).val(localStorage.getItem("savy-"+this.id).split(","));
+            if(localStorage.getItem(sv+this.id)){
+              $(this).val(localStorage.getItem(sv+this.id).split(","));
             }
             $(this).change(function() {
-              localStorage.setItem("savy-"+this.id, $(this).val());
+              localStorage.setItem(sv+this.id, $(this).val());
             });
           }else{
-            if(localStorage.getItem("savy-"+this.id)){
+            if(localStorage.getItem(sv+this.id)){
               $(this).val(localStorage.getItem("savy-"+this.id));
             }
             $(this).change(function() {
-              localStorage.setItem("savy-"+this.id, $(this).val());
+              localStorage.setItem(sv+this.id, $(this).val());
             });
           }
         }
       });
-      if ($.isFunction(fn)) {
-        fn();
-      }
+      if ($.isFunction(fn)){fn();}
     }else if (order == "destroy") {
       $(this).each(function() {
-        if(localStorage.getItem("savy-"+this.id)){
-          localStorage.removeItem("savy-"+this.id)
+        if(localStorage.getItem(sv+this.id)){
+          localStorage.removeItem(sv+this.id)
         }
       });
-      if ($.isFunction(fn)) {
-        fn();
-      }
+      if ($.isFunction(fn)){fn();}
     }else{
       console.error("savy action not defined please use $('.classname').savy('load') to trigger savy to save all inputs")
     }
